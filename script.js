29e90e629d642061f7434b8f75f743229e6ab3f9
ItsCR24 <88537135+ItsCR24@users.noreply.github.com>
2024-11-25 20:06:43 +0100

    Initial commit. Shoutout to Mr. John

---
+++ script.js
@@ -0,0 +1,21 @@
+function buy(cost) {
+    const h2 = document.getElementById("money");
+    let moneyValue = parseInt(h2.textContent.replace("$", ""), 10);
+    if (moneyValue > cost) {
+        let newValue = moneyValue - cost;
+
+        if (newValue < 0) {
+            newValue = 0;
+        }
+
+        h2.textContent = "$" + newValue;
+    }
+}
+
+function sell(cost) {
+    const h2 = document.getElementById("money");
+    let moneyValue = parseInt(h2.textContent.replace("$", ""), 10);
+    if (!isNaN(moneyValue)) {
+        h2.textContent = "$" + (moneyValue + cost);
+    }
+}
