8e49281427e84c528cc1ea5cf87b6ea1ed42fac9
ItsCR24 <88537135+ItsCR24@users.noreply.github.com>
2024-12-06 16:12:22 +0100

    Added a receipt :)

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
-function buy(cost) {
+const items = {}; 
+
+function buy(price, name) {
     const h2 = document.getElementById("money");
     let moneyValue = parseInt(h2.textContent.replace("$", ""), 10);
-    if (moneyValue >= cost) {
-        let newValue = moneyValue - cost;
+    if (moneyValue >= price) {
+        let newValue = moneyValue - price;
 
         if (newValue <= 0) {
             newValue = 0;
         }
+
         new Audio("assets/sounds/buy.wav").play();
         h2.textContent = "$" + newValue;
+
+        if (!items[name]) items[name] = { count: 0, price: price };
+            items[name].count++;
+            updateReceipt();
     }
 }
 
-function sell(cost) {
+function sell(price, name) {
     const h2 = document.getElementById("money");
     let moneyValue = parseInt(h2.textContent.replace("$", ""), 10);
-    if (!isNaN(moneyValue)) {
-        h2.textContent = "$" + (moneyValue + cost);
+    if (moneyValue != 0) {
+        h2.textContent = "$" + (moneyValue + price);
+
+        if (items[name] && items[name].count > 0) {
+            items[name].count--;
+            updateReceipt();
+        }
     }
 }
+
+    function updateReceipt() {
+        const container = document.getElementById("receipt-table");
+        container.innerHTML = '';
+        let totalValue = 0;
+        let hasItems = false;
+
+        const table = document.createElement("table");
+
+        for (const [name, data] of Object.entries(items)) {
+            if (data.count > 0) {
+                hasItems = true;
+                const row = document.createElement("tr");
+                row.innerHTML = `<td>${name}</td> <td>${data.count}x</td> <td>$${data.price * data.count}</td>`;
+                table.appendChild(row);
+                totalValue += data.price * data.count;
+            }
+        }
+
+        if (hasItems) {
+            container.appendChild(table);
+            container.appendChild(document.createElement("hr"));
+            container.innerHTML += `<table><tr><td>TOTAL</td><td> $${totalValue} </td></tr></table>`;
+        } else {
+            container.querySelector(".message").style.display = "block";
+        }
+    }
